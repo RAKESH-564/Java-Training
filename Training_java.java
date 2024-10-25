97c2f2b894d0a2a61a076dd77f27acd947b91e0c
RAKHI <rakeshgamidi564@gmail.com>
2024-10-25 15:16:47 +0530

    Add files via upload
    BASIC JAVA PROGRAMS CLASS-1 (25/10/2024)

---
+++ Training_java.java
@@ -0,0 +1,127 @@
+// reading the user input and printing it ==================================
+// import java.util.Scanner;
+// class Training_java{
+//     public static void main(String[] args) {
+//         Scanner scan =new Scanner(System.in);
+//         System.out.println("enter the string: ");
+//         String x = scan.nextLine(); 
+//         System.out.println("You Entered :" +x);
+//     }
+// }
+// ===========================================================================
+// import java.util.Scanner;
+// class Training_java{
+//     public static void main(String[]args){
+//         int a=10,b=20,sum;
+//         sum=a+b;
+//         System.out.println("sum of a and b is :" +sum);
+//     }
+// }
+// ============================================================================
+// DATA TYPES IN JAVA ARE int,short,byte,long,float,double,char=================
+// type csting in java =========================
+// int l=10;      assume that int data type 
+// long l=l;     data will be changed to long data type
+// float f=l;   
+// ============================================================================
+// import java.util.Scanner;
+// class Training_java{
+//     public static void main(String[] args) {
+//         int a,b,sum;
+//         Scanner scan =new Scanner(System.in);
+//         System.out.println("enter the a value: ");
+//         a = scan.nextInt(); 
+//         System.out.println("enter the b value: ");
+//         b = scan.nextInt(); 
+//         sum=a+b;
+//         System.out.println("sum of two numbers is:" +sum);
+//     }
+// }
+// ==========================================================================   
+// class Training_java{
+//     public static void main(String[]args)
+//     {
+//         String customerType="regular";
+//         int quantity=2;
+//         int totalUnit=100;
+//         int totalCost=0;
+//         int totalCost1=0;
+//         int totalCost2=0;
+//         int deleveryCharges=5;
+//         int discount=5;
+//         totalCost=totalUnit*quantity;
+//         if(customerType=="reg")
+//         {
+//         totalCost1=totalCost-(totalCost*discount/100);
+//         //    System.out.println("eligible for discount"); 
+//         System.out.println("Total Cost is:" +totalCost);
+//         System.out.println("eligible for discount"); 
+//         System.out.println("Total Cost after discount is :" +totalCost1);
+//         }
+//         else{
+//             totalCost2=totalCost+deleveryCharges;
+//             System.out.println("customer is not  eligible for discount");
+//             System.out.println("total cost: "+totalCost2);
+//         }
+        
+//     }
+// }
+// =========================================================================================================
+// TO PRINT NUMBER IS EVEN OR ODD
+// class Training_java {
+//     public static void main(String[]args){
+//         int a=7;
+//         if(a%2==0){
+//             System.out.println("a is even");
+//         }
+//         else{
+//             System.out.println("a is odd");
+//         }
+//     }
+// }
+// ====================================================================
+// TO FIND YEAR I LEAP YEAR 
+// class Training_java {
+//     public static void main(String[]args){
+//         int a=2016;
+//         if(a%4==0){
+//             System.out.println("a is LEAP YEAR ");
+//         }
+//         else{
+//             System.out.println("a NOT A LEAP YEAR");
+//         }
+//     }
+// }
+// ======================================================================
+// TO FIND TH BIGGEST OF THREE NUMBERS
+// ======================================================
+//  public class Training_java {
+//     public static void main(String[] args) 
+//     {
+//         int a = 30;
+//         int b = 20;
+//         int c = 45;
+
+//         // Using ternary operator to find the greatest number
+//         int greatest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+//         System.out.println("The greatest number is: " + greatest);
+//     }
+//  }
+//  =============================================================
+// to find the given number is perfect square
+// import java.math.*;
+// class Training_java{
+//     public static void main(String[]args){
+//         double n=36;
+//         double s=Math.sqrt(n);
+//         if((s*s)==n){
+//             System.out.println(n+ "  is perfect square");
+//         }
+//         else{
+//             System.out.println(n+ "  is not perfect square");
+//         }
+
+//     }
+// }
+// ===================================================================
+
